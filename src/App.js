45d39dfb5fcb0d4b1727cdaa3290f355b8e4f671
Bárbara Romero <bqromero@gmail.com>
2022-01-16 10:58:55 -0300

    Estilizar cards

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
 import React from "react";
 import ComponenteComFilhos from "./componentes/ComponenteComFilhos";
-import ComParametro from "./componentes/basico/ComParametro";
+import ComParametro from "./componentes/ComponenteComProps";
 import Card from "./componentes/layouts/Card";
 import "./App.css";
+import ComponenteComProps from "./componentes/ComponenteComProps";
 
 export default (props) => (
   <div className="App">
-    <Card titulo="#02 - Componente Com Parametro" color="#FF85CB">
-      <ComParametro titulo="Titulo" subtitulo="Subtitulo"></ComParametro>
+      <Card titulo="#02 - Componente Com Parametro" color="#836FFF">
+        <ComponenteComProps titulo="Grey's Anatomy" subtitulo="Série Televisiva"></ComponenteComProps>
+      </Card>
+    <Card titulo="#03 - Componente Com Filhos" color="#7FFFD4">
+      <ComponenteComFilhos>
+        <ul>
+          <li>Meredith</li>
+          <li>Alex</li>
+          <li>Geoge</li>
+          <li>Izzie</li>
+          <li>Cristina</li>
+        </ul>
+      </ComponenteComFilhos>
     </Card>
-    <Card titulo="#03 - Componente Com Filhos" color="#D96459" />
-    <ComponenteComFilhos>
-      <ul>
-        <li>Meredith</li>
-        <li>Alex</li>
-        <li>Geoge</li>
-        <li>Izzie</li>
-        <li>Cristina</li>
-      </ul>
-    </ComponenteComFilhos>
   </div>
 );
